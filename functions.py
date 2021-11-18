505f2e36911d54c345b58bc9ced69290cfcdf2cb
ajhaap <aj.haapanen@outlook.com>
2021-11-18 21:47:01 +0200

    get_btc_data now returns the correct list.

--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@ import json
 import requests
 from datetime import datetime
 
+
 def get_timestamp(date):
 
     string = f'{date} 01:00:00.000000 +0000'
@@ -10,20 +11,24 @@ def get_timestamp(date):
 
     return int(timestamp)
 
-def get_btc_data(ts_startdate, ts_enddate):
+
+def get_btc_data(ts_startdate, ts_enddate, startdate):
+
+    date_index = False
+    list_index = ''
 
     if (ts_enddate - ts_startdate) < 7776001:
         ts_startdate = int(ts_enddate - 7776001)
+        date_index = True
 
     r = requests.get(
-        f'https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=eur&from={ts_startdate}&to={ts_enddate}',
+        f'https://api.coingecko.com/api/v3/coins/bitcoin/market_chart'
+        f'/range?vs_currency=eur&from={ts_startdate}&to={ts_enddate}'
         )
 
     data = json.loads(r.content)
 
-    prices = [x[1] for x in data['prices']]
     dates = [x[0] for x in data['prices']]
-    volumes = [x[1] for x in data['total_volumes']]
     utcdates = []
 
     for date in dates:
@@ -31,9 +36,46 @@ def get_btc_data(ts_startdate, ts_enddate):
         utcdate = datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d')
         utcdates.append(utcdate)
 
-    btcdict = [{'date': utcdate, 'price': price, 'volume': volume}
-               for utcdate,price,volume in zip(utcdates,prices,volumes)]
+    if date_index:
+        list_index = utcdates.index(str(startdate))
+        utcdates = utcdates[list_index:]
+
+    prices = [x[1] for x in data['prices']]
+    if date_index:
+        prices = prices[list_index:]
+
+    volumes = [x[1] for x in data['total_volumes']]
+    if date_index:
+        volumes = volumes[list_index:]
+
+    keys = ['date', 'price', 'volume']
+    values = [utcdates, prices, volumes]
+    btcdict = dict(zip(keys, values))
 
     return btcdict
 
 
+def get_variables(dictionary):
+    tobuy_date, tosell_date = '', ''
+    highest = max(dictionary['volume'])
+    max_price = dictionary['price'][0]
+    min_price = max_price
+    profit = 0
+    min_index, max_index = 0, 0
+
+    index = 0
+    for x in dictionary['price']:
+        index += 1
+        if x - min_price > profit and x > min_price:
+            profit = x - min_price
+            max_index = index
+        if x < min_price:
+            min_index = index
+            min_price = x
+
+    tobuy_date = dictionary['date'][min_index]
+    tosell_date = dictionary['date'][max_index]
+
+    return print(f'Hightest volume was on {highest}\n'
+                 f'For maximum profits, one should buy on {tobuy_date}'
+                 f' and sell on {tosell_date}')
--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 import functions
 
-startdate = '2021-11-11'
+startdate = '2021-09-11'
 ts_startdate = functions.get_timestamp(startdate)
 
 enddate = '2021-11-15'
 ts_enddate = functions.get_timestamp(enddate)
 
-btcdict = {}
+btcdict = functions.get_btc_data(ts_startdate, ts_enddate, startdate)
 
-list = functions.get_btc_data(ts_startdate, ts_enddate)
-
-print(list)
-
-
-
-
-
-
-
-
+functions.get_variables(btcdict)
