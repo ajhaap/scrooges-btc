c010f9e1ce69c6fbc269e51d5529caf7c9b589b5
ajhaap <aj.haapanen@outlook.com>
2021-11-22 21:02:23 +0200

    second iteration of web app for pythoneverywhere

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 app.add_url_rule('/', view_func=views.index)
 app.add_url_rule('/result', view_func=views.result, methods=['POST'])
 
-app
 
 if __name__ == "__main__":
     app.run()
